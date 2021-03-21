0590d6a0dc0d94f5c23180b13316a4891f840b52
Rohan Samanta <42403165+espatatis@users.noreply.github.com>
2021-03-21 22:56:54 +0530

    Delete readaholic directory

--- readaholic/shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views 
-
-
-urlpatterns = [
-    path('', views.home, name = 'shop-home'),
-    path('shop/<int:book_id>/', views.detail, name = 'detail')
-]
